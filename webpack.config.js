0cf05e7016f1391f2fe424ea24c4c73a2e0f8d9d
Mol0D <moloduk99@gmail.com>
2021-11-08 11:22:18 +0200

    add more settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDevMode = process.env.NODE_ENV === 'development';
 
 module.exports = {
+    stats: 'minimal',
+    devtool: isDevMode ? 'source-map' : false,
     devServer: {
         port: 4200,
     },
@@ -14,6 +21,16 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: '/src/index.html'
         }),
+        new CleanWebpackPlugin(),
+        // new CopyPlugin({ // uncomment when will add file to assets folder
+        //    patterns: [
+        //        {
+        //            from: 'src/assets/**/*',
+        //            to: 'dest/'
+        //        }
+        //    ]
+        // }),
+        new MiniCssExtractPlugin(),
     ],
     module: {
         rules: [
@@ -30,11 +47,26 @@ module.exports = {
             {
                 test: /\.sass*$/,
                 use: [
-                    'style-loader',
+                    MiniCssExtractPlugin.loader,
                     'css-loader',
                     'sass-loader',
                 ]
-            }
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.svg/,
+                use: {
+                    loader: "svg-url-loader",
+                    options: {},
+                },
+            },
         ]
     }
 }
